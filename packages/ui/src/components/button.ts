417206b992ad5064e954ea426f466752a2997cda
ltg <ltg@bbangyanetworks.com>
2023-09-01 06:28:36 +0900

    first Commit

---
+++ packages/ui/src/components/button.ts
@@ -0,0 +1,34 @@
+import { defineComponent, h, isVue2 } from 'vue-demi'
+
+export default defineComponent({
+  name: 'ButtonItem',
+  props: {
+    onClick: {
+      type: Function,
+      default: () => {
+        console.log('default')
+      },
+    },
+  },
+  render() {
+    if (isVue2) {
+      return h(
+        'button',
+        {
+          on: {
+            click: this.onClick,
+          },
+        },
+        this.$slots.default ?? 'test'
+      )
+    }
+
+    return h(
+      'button',
+      {
+        onClick: this.onClick,
+      },
+      this.$slots.default ? this.$slots.default() : 'test'
+    )
+  },
+})
